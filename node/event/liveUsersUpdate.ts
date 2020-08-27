98dd47c7e74ba4cd1bc49e30c43cdb64c6678965
Marcos Campelo <m.campelo@samsung.com>
2020-08-27 12:02:55 -0300

    [Issue 5] event triggering analytics

--- node/event/liveUsersUpdate.ts
+++ node/event/liveUsersUpdate.ts
@@ -1,3 +1,9 @@
-export async function updateLiveUsers() {
-    console.log('EVENT HANDLER: received event')
+import { Clients } from '../clients/index'
+import Analytics from '../clients/analytics'
+import { EventContext } from '@vtex/api'
+
+export async function updateLiveUsers(ctx: EventContext<Clients>) {
+    const liveUsersProducts = await (ctx.clients.analytics as Analytics).getLiveUsers();
+    console.log('LIVE USERS: ', liveUsersProducts)
+    return true
 }
