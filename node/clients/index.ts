6587badc23a90a94b90b09e93120b9177fe518f3
Marcos Campelo <m.campelo@samsung.com>
2020-08-27 11:29:56 -0300

    [Issue 4] Analytics

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,7 +1,10 @@
-import { IOClients } from '@vtex/api'
-
+import { IOClients, IOClientConstructor } from '@vtex/api'
+import Analytics from './analytics'
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
-
+    public get analytics() {
+        console.log('ANALYTICS: request received')
+        return this.getOrSet('analytics', Analytics as IOClientConstructor);
+    }
 }
--- node/handlers/analytics.ts
+++ node/handlers/analytics.ts
@@ -1,9 +1,15 @@
+import Analytics from '../clients/analytics'
+
 export async function analytics(ctx: Context, next: () => Promise<any>) {
   if (ctx.method.toUpperCase() === 'GET') {
-    ctx.status = 200
-    ctx.body = 'OK'
+    const {
+      clients: { analytics }
+    } = ctx;
 
+    ctx.status = 200;
+    ctx.body = await (analytics as Analytics).getLiveUsers();
     ctx.set('cache-control', 'no-cache')
   }
+
   await next()
 }
